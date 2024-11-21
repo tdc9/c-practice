0815caa40ce7b408d1066d8e38fdda057090c80c
Jayesh Gaur <sempai@animehaven.fun>
2024-11-21 12:28:47 +0530

    completing the course

---
+++ writingfiles.c
@@ -0,0 +1,22 @@
+int main()
+{
+   // WRITE/APPEND A FILE
+   FILE *pF = fopen("C:\\Users\\Cakow\\Desktop\\test.txt", "w");
+
+   fprintf(pF, "Spongebob Squarepants");
+
+   fclose(pF);
+   
+   // DELETE A FILE
+   /*
+   if(remove("test.txt") == 0)
+   {
+      printf("That file was deleted successfully!");
+   }
+   else
+   {
+      printf("That file was NOT deleted!");
+   }
+   */
+   return 0;
+}
