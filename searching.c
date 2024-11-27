6c18420d3981530388555ab353a55b85f1a1816a
Jayesh Gaur <sempai@animehaven.fun>
2024-11-27 16:22:03 +0530

    searching.c

---
+++ searching.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){ 
+    int arr[]={1,2,3,4,5,6,7,8,9,10}
+    int key;
+    int size = sizeof(arr)/sizeof(int); 
+    int i=0;
+    char choice;
+    while(i=0){
+        printf("Choose You Option Between 1-3");
+
+
+    
+    
+}
